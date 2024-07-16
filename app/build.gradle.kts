e51b0943427ab60ee4c6416b1d24417f678f2563
Ankit litoriya <ankitlitoriya17@gmail.com>
2024-07-16 15:53:25 +0530

    initialized app on firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,9 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.firebase.database)
+    implementation(libs.support.annotations)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(platform(libs.firebase.bom))
 }
